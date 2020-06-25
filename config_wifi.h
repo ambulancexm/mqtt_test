b9153f953c9f2658b2f39a2a8ec5017187fd0fe2
Thomas BEREZIAT <bereziat.th@gmail.com>
2020-06-25 21:33:56 +0200

    premier commit

---
+++ config_wifi.h
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+
+class configWifi
+{
+  private: 
+    char* ssid;
+    char* pwd;
+    char* mqqt_broker;
+    int numWifi;
+    volatile int nbWifi = 0;
+  
+  public:
+    configWifi(char* _ssid,char* _pwd, char* _mqqt_broker);
+    char* getAP();
+    char* getSsid();
+    char* getPwd();
+    char* getMqqt_broker();
+    char* getNumWifi();
+    
+};
+
+configWifi::configWifi(char* _ssid,char* _pwd,char* _mqqt_broker){
+  nbWifi++;
+  numWifi= nbWifi;
+  ssid = _ssid;
+  pwd = _pwd;
+  mqqt_broker = _mqqt_broker;
+}
+
+char* configWifi::getAP(){
+  char rt[255];
+  strcat(rt,ssid);
+  strcat(rt,";");
+  strcat(rt,pwd);
+  strcat(rt,";");
+  strcat(rt,mqqt_broker);
+  strcat(rt,";");
+  strcat(rt,getNumWifi());
+  strcat(rt,";");
+  
+  return rt;
+}
+
+char* configWifi::getSsid(){
+  return ssid;
+}
+
+char* configWifi::getPwd(){
+  return pwd;
+}
+
+char* configWifi::getMqqt_broker(){
+  return mqqt_broker;
+}
+
+char* configWifi::getNumWifi(){
+  char buffer[5];
+  strcpy(buffer,"");
+  //iota(1,buffer,10);
+  sprintf(buffer, "%d", numWifi);
+  return buffer;
+}
---
+++ mqtt_test.ino
@@ -0,0 +1,131 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h> 
+#include <PubSubClient.h> //Librairie pour la gestion Mqtt 
+#include <stdlib.h>
+#include "config_wifi.h"
+ 
+//WIFI
+const char* ssid = "thomas";
+const char* password = "";
+
+// liste de wifi  ssid -- password -- mqtt_server IP Broker -- Broker port 
+configWifi huawai("thomas","tiliatilia","192.168.43.109");
+
+//MQTT
+const char* mqtt_server = "192.168.43.109";//Adresse IP du Broker Mqtt
+const int mqttPort = 1883; //port utilisé par le Broker 
+long tps=0;
+bool tmp = 0;
+long nb=0; 
+ESP8266WiFiMulti WiFiMulti;
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+  setup_wifi();
+  setup_mqtt();
+  client.publish("loc/testMQ", "Hello from ESP8266");
+}
+ 
+void loop() {
+  
+  char info[255];
+  reconnect();
+  client.loop(); 
+  //On utilise pas un delay pour ne pas bloquer la réception de messages 
+  if (millis()-tps>10000){
+     tps=millis();
+     float temp = random(30);
+     temp = 1;
+     tmp = !tmp;
+     //mqtt_publish("pub/1ab",temp);
+     mqtt_publish("pub/13ab",tmp);
+     
+     Serial.print("qqchose : ");
+     Serial.println(temp); 
+   }
+   strcpy(info,"");
+   strcat(info,huawai.getAP());
+   
+   
+   client.publish("loc/tmpTest", info);
+   nb++;
+   //strcpy(info,"");
+   delay(1500);
+}
+ 
+void setup_wifi(){
+  //connexion au wifi
+  WiFiMulti.addAP(huawai.getSsid(), huawai.getPwd());
+  while ( WiFiMulti.run() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+  Serial.println("");
+  Serial.println("WiFi connecté");
+  Serial.print("MAC : ");
+  Serial.println(WiFi.macAddress());
+  
+  Serial.print("Adresse IP : ");
+  Serial.println(WiFi.localIP());
+ 
+}
+ 
+void setup_mqtt(){
+  client.setServer(mqtt_server, mqttPort);
+  client.setCallback(callback);//Déclaration de la fonction de souscription
+  reconnect();
+}
+ 
+//Callback doit être présent pour souscrire a un topic et de prévoir une action 
+void callback(char* topic, byte *payload, unsigned int length) {
+   Serial.println("-------Nouveau message du broker mqtt-----");
+   Serial.print("Canal:");
+   Serial.println(topic);
+   Serial.print("donnee:");
+   Serial.write(payload, length);
+   Serial.println();
+   if(topic == "sensor/temperature"){
+    Serial.println("reception ok sensor ");
+   }
+      
+   if ((char)payload[0] == '1') {
+     Serial.println("LED ON");
+      digitalWrite(13,HIGH); 
+   } else {
+     Serial.println("LED OFF");
+     digitalWrite(13,LOW); 
+   }
+   
+ }
+ 
+ 
+void reconnect(){
+  while (!client.connected()) {
+    Serial.println("Connection au serveur MQTT ...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("MQTT connecté");
+    }
+    else {
+      Serial.print("echec, code erreur= ");
+      Serial.println(client.state());
+      Serial.println("nouvel essai dans 2s");
+    delay(2000);
+    }
+  }
+   client.subscribe("sensor/temperature");//souscription au topic led pour commander une led
+   client.subscribe("pub/13a");
+   
+}
+ 
+//Fonction pour publier un float sur un topic 
+void mqtt_publish(String topic, float t){
+  char top[topic.length()+1];
+  topic.toCharArray(top,topic.length()+1);
+  char t_char[50];
+  String t_str = String(t);
+  t_str.toCharArray(t_char, t_str.length() + 1);
+  client.publish(top,t_char);
+}
